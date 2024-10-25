97ee5227348e8b84478c90cbdb50488678f34efe
bs f <304446350@qq.com>
2024-10-25 14:28:24 +0800

    commit

---
+++ src/main/java/boshen/algorithm/dayone.java
@@ -0,0 +1,14 @@
+package boshen.algorithm;
+
+/**
+ * @Classname boshen.algorithm.dayone
+ * @Description
+ * @Date 2024/10/25 13:36
+ * @Author: boshen
+ */
+public class dayone {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
