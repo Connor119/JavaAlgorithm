01ef0a891d7155f2972ff00f645b37236361e6a4
bs f <304446350@qq.com>
2024-10-25 13:41:36 +0800

    first commit

---
+++ src/main/java/dayone.java
@@ -0,0 +1,12 @@
+/**
+ * @Classname dayone
+ * @Description
+ * @Date 2024/10/25 13:36
+ * @Author: boshen
+ */
+public class dayone {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
